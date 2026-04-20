711d35c47cc7c8ae9e67a308011963817cc65924
SANTIAGO <SANTIAGO@DESKTOP-BBHOA2A>
2026-04-19 23:07:00 -0500

    Correccion para pagina web4.0

--- config/routes.php
+++ config/routes.php
@@ -55,7 +55,7 @@ return function (RouteBuilder $routes): void {
          * its action called 'display', and we pass a param to select the view file
          * to use (in this case, templates/Pages/home.php)...
          */
-        $builder->connect('/', ['controller' => 'Ventas', 'action' => 'display', 'comisiones']);
+        $builder->connect('/', ['controller' => 'Ventas', 'action' => 'comisiones']);
 
         /*
          * ...and connect the rest of 'Pages' controller's URLs.
