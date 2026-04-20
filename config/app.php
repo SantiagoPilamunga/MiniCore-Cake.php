cce9f8ff88aef760c7ead250cf95a2ad4d418b73
SANTIAGO <SANTIAGO@DESKTOP-BBHOA2A>
2026-04-20 01:12:55 -0500

    Correccion 7

--- config/app.php
+++ config/app.php
@@ -286,6 +286,7 @@ return [
          * in app_local.php depending on the application's needs.
          */
         'default' => [
+            'url' => env('DATABASE_URL', null),
             'className' => Connection::class,
             'driver' => Mysql::class,
             'persistent' => false,
